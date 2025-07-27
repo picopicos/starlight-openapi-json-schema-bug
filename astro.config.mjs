87cfcf9215d66884e6c6309b20b2971aa9281eff
picopicos <picopicoico@gmail.com>
2025-07-27 22:13:16 +0900

    reproduce bug

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,6 @@
 import starlight from '@astrojs/starlight'
 import { defineConfig } from 'astro/config'
+import starlightOpenAPI, { openAPISidebarGroups } from 'starlight-openapi'
 
 export default defineConfig({
   site: 'https://picopicos.github.io',
@@ -7,12 +8,21 @@ export default defineConfig({
   integrations: [
     starlight({
       plugins: [
+        // Generate the OpenAPI documentation pages.
+        starlightOpenAPI([
+          {
+           base: 'api',
+           schema: 'schemas/api-schema.yaml',
+          },
+        ]),
       ],
       sidebar: [
         {
           label: 'Guides',
           items: [{ label: 'Example Guide', link: '/guides/example/' }],
         },
+        // Add the generated sidebar group to the sidebar.
+        ...openAPISidebarGroups,
       ],
       title: 'My Docs',
     }),
